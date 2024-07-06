357582e72b0e800b8d479829e8f89df9bf5700d2
KingClam <cameronkobeadams@gmail.com>
2024-07-05 22:24:53 -0400

    added the jump function

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -9,8 +9,14 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]
     private GameObject groundCheck;
+
+    [SerializeField]
+    private Camera MainCam;
+
+    [SerializeField]
     private float movementSpeed, acceleration, gravityAccel = 10f, maxMovementSpeed, currentVelocity, moveDirection, jumpVelocity = 5f;
     public bool isMoving, isGrounded, gravityAffected;
+    private int groundLayer = 7;
     private Vector2 moveVector;
 
     // Start is called before the first frame update
@@ -21,7 +27,7 @@ public class PlayerController : MonoBehaviour
         maxMovementSpeed = 5f;
         currentVelocity = 0;
         rigidbody = GetComponent<Rigidbody2D>();
-        isGrounded = false;
+        isGrounded = true;
         moveVector = new Vector2(0f,0f);
     }
 
@@ -40,25 +46,42 @@ public class PlayerController : MonoBehaviour
         }
 
         rigidbody.velocity = new Vector2(maxMovementSpeed * moveVector.x, rigidbody.velocity.y);
-
+        MainCam.transform.position = new Vector3(this.transform.position.x, MainCam.transform.position.y, MainCam.transform.position.z);
+        //isGrounded = Physics.CheckSphere(groundCheck.transform.position,.2f,lay)
 
     }
 
     public void MovementPerformed(InputAction.CallbackContext context)
     {
         moveVector = new Vector2(context.ReadValue<float>(), moveVector.y);
+        
 
     }
 
     public void GroundCheck()
     {
-        if(isGrounded)
-        {
 
+        Debug.Log(groundCheck.GetComponent<CircleCollider2D>().IsTouchingLayers(LayerMask.GetMask("Ground")));
+        if(groundCheck.GetComponent<CircleCollider2D>().IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            isGrounded = true; 
         }
         else
         {
             isGrounded = false;
+        }
+
+    }
+
+    public void Jump(InputAction.CallbackContext context) 
+    {
+        if(context.performed)
+        {
+            GroundCheck();
+            if(isGrounded)
+            {
+                rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpVelocity);
+            }
         }
     }
 }
