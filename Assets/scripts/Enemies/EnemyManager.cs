f4cda6365971a5a741ad579c085e6d44d43b78f1
KingClam <cameronkobeadams@gmail.com>
2025-04-29 23:10:04 -0400

    Update EnemyManager.cs

--- Assets/scripts/Enemies/EnemyManager.cs
+++ Assets/scripts/Enemies/EnemyManager.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     public static EnemyManager instance { get; private set; }
+
+    List<GameObject> enemyList;
+
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+
+        enemyList = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void EntityHurtSearch()
+    {
+        //Searches through the enemy list for an entity that collides with a hit box the player made
     }
 }
